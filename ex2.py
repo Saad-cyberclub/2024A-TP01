00e43f7427c33a1ef87d2cecf42e1c54e212ab89
Anas <anashrc8@gmail.com>
2024-09-13 14:40:24 -0400

    exercices

--- ex2.py
+++ ex2.py
@@ -1,4 +1,21 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
+import math
+water_quantity = 0
+water_quantity =float(input('Quelle quantité d\'eau faut-il assainir ? '))
+filtre =  math.ceil(water_quantity/5)
+lampes =math.ceil((water_quantity*3)/5)
+chlore = (water_quantity*(1/2))/5
+#print('Voici les éléments requis pour assainir ',water_quantity,'L d\'eau:') 
+#print('               - Filtre(s) :', filtre)
+#print('-\tLampe(s) UV :' , lampes,)
+#print('-\tChlore :', chlore, 'kg')
 
-water_quantity = ...
+a= f"""Voici les éléments requis pour assainir {water_quantity}L d'eau:\n
+        \t- Filtre(s) : {filtre}\n
+        \t- Lampe(s) UV : {lampes}\n
+        \t- Chlore : {chlore}kg\n"""
+print(a)
+
+
+
--- ex3.py
+++ ex3.py
@@ -1,5 +1,14 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
 
-speed = ... 
-angle = ...
+import math
+speed = 0
+angle = 0
+speed = float(input('Vitesse initiale (m/s): '))
+angle = float(input('Angle de lancer (en degrés): '))
+
+
+sin = (angle*3.14)/180
+distance = ((speed**2)*(math.sin(sin*2)))/9.8
+
+print ('Distance parcourue: ', round(distance , 2),'m' )
