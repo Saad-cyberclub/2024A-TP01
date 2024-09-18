253a1bb2887620ef7d7ccbd4a7f319a166fc3fc1
Anas <anashrc8@gmail.com>
2024-09-18 15:54:17 -0400

    ex5

--- ex5.py
+++ ex5.py
@@ -1,5 +1,22 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
 
-country = ...
-code_medals = ...
+country = input('Pays concerné ? ')
+code_medals = input('Chaine représentant les médailles ? ')
+x=0
+y=0
+z=0
+for i in (code_medals):
+    if i=='G':
+        x= x+1
+    elif i=='S':
+        y= y + 1
+    elif i=='B':
+        z= z+1
+    else: 
+        print('Veuillez entrer des lettres correctes')
+
+print(country+':')
+print('-',  x,'OR' )
+print('-',  y,'Argent')
+print('-', z,'Bronze')
